b0e245d637aa738ad9e4668047610fefba77c7e7
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-02-18 20:39:59 +0100

    Set up window icon

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Chess exercises organizer
 
 Organize your favorite chess positions and play them against the computer at its highest level.
+
+## Credits
+
+Knight icon downloaded from http://www.iconarchive.com/show/mac-icons-by-artua/Chess-icon.html.
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # coding: utf-8
  
-from tkinter import Tk
+from tkinter import Tk, PhotoImage
 from chess_board import ChessBoard
 
 window = Tk()
 window.resizable(False, False)
 window.title("Chess Exercises Organizer")
+window.tk.call('wm', 'iconphoto', window._w, PhotoImage(file='knight.png'))
 
 chess_board = ChessBoard(window)
 
