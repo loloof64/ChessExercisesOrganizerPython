11617d9621838bec79c0b6749c93775c37627487
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-02-18 20:29:56 +0100

    Slightly improved main window

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from tkinter import Tk
 from chess_board import ChessBoard
 
 window = Tk()
+window.resizable(False, False)
+window.title("Chess Exercises Organizer")
 
 chess_board = ChessBoard(window)
 
